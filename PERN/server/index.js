d923bb37fee9258c05bc99badcef3f33f4b1b6d9
Khalid <khalid20031016@gmail.com>
2024-02-06 12:22:31 +0500

    Update index.js

--- PERN/server/index.js
+++ PERN/server/index.js
@@ -5,6 +5,26 @@ const pool = require("./db");
 //midle ware
 app.use(cors());
 app.use(express.json());
+
+//Routes//
+
+//create a todo
+app.post("/todos", async(req, res)=>{
+    try{
+
+        console.log(req.body)
+
+    } catch (err){
+        console.error(err.message);
+    }
+}
+)
+//get all todos
+
+// update a todo
+
+//delete a todo
+
 app.listen(5000, ()=>{
     console.log("server has started on port 5000");
 })
